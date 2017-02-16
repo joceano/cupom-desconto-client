4b4f9c7d8008a2007644fcf6a7217f264ff12aee
joceano <alves.joceano@gmail.com>
2017-02-16 07:44:39 -0200

    Inicio da utilização do md-data-table

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 (function(angular) {
 	'use strict';
-	angular.module('app', ['app.controllers', 'app.routes', 'app.services', 'app.directives']);
+	angular.module('app', ['app.controllers', 'app.routes', 'app.services', 
+						   'app.directives', 'md.data.table']);
 })(window.angular);
