fc81d42532501886a24381a88f1d76cca6403071
joceano <alves.joceano@gmail.com>
2017-05-22 23:11:47 -0300

    Iniciado o Home
    Iniciado o Home e criado os card's em todas as listas com as pesquisas.

--- js/controllers/homeController/homeController.js
+++ js/controllers/homeController/homeController.js
@@ -1,9 +1,20 @@
 (function (angular) {
 	'use strict';
 
-	angular.module('app.controllers').controller('HomeController', ['$scope', 
-	 function(scope) {
-	 	scope.nome = "Home";	 			 
+	angular.module('app.controllers').controller('HomeController', ['$scope', 'HttpService',
+	 function(scope, httpService) {
+	 	
+	 	var getAnuncios = function() {
+            scope.loading = true;
+            httpService.get('/home/').then(function(res) {                
+                scope.anuncios = res.data; 
+                scope.loading = false;                
+            }, function (error) {                
+                toastAlert.defaultToaster('Ops, não foi possível carregar os anúncios.');
+            });
+        };	 
+
+        getAnuncios();			 
 
 	}]);
 
